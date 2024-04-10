e7e1f216e65b0fd0614f91cd70bddfa04dc35acb
dany61955 <98032858+dany61955@users.noreply.github.com>
2024-04-10 15:07:39 +0530

    Update asa_cfg_push.py

--- asa_cfg_push.py
+++ asa_cfg_push.py
@@ -12,16 +12,19 @@ def push_config(hostname, username, password, enable_password, config_lines):
         ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh_client.connect(hostname, username=username, password=password, look_for_keys=False, allow_agent=False)
 
+        # Create shell
+        ssh_shell = ssh_client.invoke_shell()
+
         # Send enable command
-        ssh_client.send("enable\n")
-        ssh_client.send(enable_password + "\n")
-        output = ssh_client.recv(65535).decode('ascii')
+        ssh_shell.send("enable\n")
+        ssh_shell.send(enable_password + "\n")
+        output = ssh_shell.recv(65535).decode('ascii')
 
         # Check if enable mode is reached
         if re.search(r'\S+#', output):
             for line in config_lines:
-                ssh_client.send(line)
-                output = ssh_client.recv(65535).decode('ascii')
+                ssh_shell.send(line)
+                output = ssh_shell.recv(65535).decode('ascii')
                 # Check for errors
                 if re.search(r'%\sError:', output):
                     print(f"Error executing line: {line.strip()}")
